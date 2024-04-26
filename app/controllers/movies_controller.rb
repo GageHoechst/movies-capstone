62abf57db3beee6d496aa3042cd9423a7218d063
GageHoechst <hoechstg99@gmail.com>
2024-04-26 16:06:30 -0400

    added the update action to movies

--- app/controllers/movies_controller.rb
+++ app/controllers/movies_controller.rb
@@ -8,4 +8,17 @@ class MoviesController < ApplicationController
     @movie = Movie.find_by(id: params[:id])
     render :show
   end
+
+  def update
+    @movie = Movie.find_by(id: params[:id])
+    @movie.update(
+      name: params[:name] || @movie.name,
+      director: params[:director] || @movie.director,
+      image_url: params[:image_url] || @movie.image_url,
+      description: params[:description] || @movie.description,
+      run_time: params[:run_time] || @movie.run_time,
+
+    )
+    render :show
+  end
 end
